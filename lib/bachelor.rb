83e4bc4e6a166960be76784fab9a58114297cd23
Hugo Escandon <hescandon6@gmail.com>
2018-07-19 19:31:44 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,6 @@
 def get_first_name_of_season_winner(data, season)
-  # code hdata[season].each do |contestant|
+  # code h
+  data[season].each do |contestant|
     if contestant["status"] == "Winner"
       return contestant["name"].split(" ")[0]
     end
